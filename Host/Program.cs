3adbd6b0dac858201e831692b27df6b5c28198c1
Jared Parsons <jared@paranoidcoding.org>
2024-12-17 17:10:32 -0800

    Jared tries COM for first time in a decade

--- Host/Program.cs
+++ Host/Program.cs
@@ -19,6 +19,7 @@
 
 using System;
 using System.Runtime.InteropServices;
+using static HRESULT;
 
 namespace Host
 {
@@ -26,13 +27,9 @@ namespace Host
     {
         public static void Main(string[] args)
         {
-            if (args.Length != 1)
-            {
-                Console.Error.WriteLine("Must supply native library to load.");
-                return;
-            }
+            var lib = @"C:\Users\jaredpar\code\GetMetaDataDispenser\artifacts\bin\getmddisp.dll";
 
-            if (!TryLoad(args[0], out nint handle))
+            if (!TryLoad(lib, out nint handle))
             {
                 Console.Error.WriteLine($"Failed to load native binary: {args[0]}.");
                 return;
@@ -45,15 +42,102 @@ namespace Host
                 return;
             }
 
-            int hr = ((delegate* unmanaged[Stdcall]<out void*, int>)export)(out void* dispenser);
+            int hr = ((delegate* unmanaged[Stdcall]<out void*, int>)export)(out void* dispenserPtr);
             if (hr < 0)
             {
                 Console.Error.WriteLine($"Failed to get dispenser instance: 0x{hr:x}.");
                 return;
             }
 
-            Console.WriteLine($"Successfully acquired dispenser instance: 0x{(nint)dispenser:x}.");
-            Marshal.Release((nint)dispenser);
+            Console.WriteLine($"Successfully acquired dispenser instance: 0x{(nint)dispenserPtr:x}.");
+
+            var dispenser = (IMetaDataDispenser)Marshal.GetObjectForIUnknown((nint)dispenserPtr);
+            Guid iidMetaDataImport2 = new("FCE5EFA0-8BBA-4f8e-A036-8F2022B08466");
+
+            var assemblyPath = @"C:\Program Files\dotnet\packs\Microsoft.WindowsDesktop.App.Ref\8.0.11\ref\net8.0\System.Windows.Forms.dll";
+            dispenser.OpenScope(assemblyPath, 0 /* read */, ref iidMetaDataImport2, out nint importPtr);
+
+            var import = (IMetaDataImport2)Marshal.GetObjectForIUnknown(importPtr);
+            Test(import);
+
+            Marshal.Release((nint)dispenserPtr);
+        }
+
+        static void Test(IMetaDataImport2 import)
+        {
+            uint[] typeDefs = new uint[1];
+            nint e = 0;
+            do 
+            {
+                var hr = import.EnumTypeDefs(ref e, typeDefs, 1, out uint found);
+                if (hr != HRESULT.S_OK)
+                {
+                    break;
+                }
+                ForTypes(import, typeDefs[0]);
+            } while (true);
+        }
+
+        static void ForTypes(IMetaDataImport2 import, uint typeDef)
+        {
+            var name = GetString(import, typeDef);
+            Console.WriteLine(name);
+
+            uint[] methodDefs = new uint[1];
+            nint e = 0;
+            do
+            {
+                var hr = import.EnumMethods(ref e, typeDef, methodDefs, 1, out uint found);
+                if (hr != HRESULT.S_OK)
+                {
+                    break;
+                }
+                ForMethods(import, typeDef, methodDefs[0]);
+            } while(true);
+
+            import.CloseEnum(e);
+        }
+
+        static void ForMethods(IMetaDataImport2 import, uint typeDef, uint methodDef)
+        {
+            var name = GetString(import, methodDef);
+            Console.Write($"\t{name}(");
+            if (name.Contains("GetOwnNeighboringToolsRectangles"))
+            {
+
+            }
+            uint[] paramDefs = new uint[5];
+            nint e = 0;
+            var first = true;
+            do
+            {
+                var hr = import.EnumParams(ref e, methodDef, paramDefs, paramDefs.Length, out uint found);
+                if (hr != HRESULT.S_OK)
+                {
+                    break;
+                }
+
+                for (int i = 0; i < found; i++)
+                {
+                    if (!first)
+                    {
+                        Console.Write(", ");
+                    }
+
+                    Console.Write(GetString(import, paramDefs[i]));
+                    first = false;
+                }
+
+            } while(true);
+            Console.WriteLine(")");
+
+            import.CloseEnum(e);
+        }
+
+        static string GetString(IMetaDataImport2 import, uint token)
+        {
+            ThrowIfFailed(import.GetNameFromToken(token, out nint namePtr));
+            return Marshal.PtrToStringAnsi(namePtr);
         }
 
         private static bool TryLoad(string name, out nint handle)
